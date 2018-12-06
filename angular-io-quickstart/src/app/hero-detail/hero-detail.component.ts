9a4a42a100656b4314433bca12465ecc122e222e
mundodehads <omundodehads@gmail.com>
2018-12-06 14:58:04 -0200

    add http integration

--- angular-io-quickstart/src/app/hero-detail/hero-detail.component.ts
+++ angular-io-quickstart/src/app/hero-detail/hero-detail.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Hero } from '../hero';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+
+import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+  styleUrls: [ './hero-detail.component.css' ]
 })
-
 export class HeroDetailComponent implements OnInit {
   @Input() hero: Hero;
 
@@ -31,5 +31,10 @@ export class HeroDetailComponent implements OnInit {
 
   goBack(): void {
     this.location.back();
+  }
+
+ save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
   }
 }
